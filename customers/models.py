742489378fe5a46fd557d82002c94a5faa75d789
m.kh <khosroshahi89@gmail.com>
2022-02-14 15:32:31 +0330

    add: address class

--- customers/models.py
+++ customers/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from core.models import BaseModel
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
-class Customer():
+
+
+class Customer(BaseModel):
+    """
+    Customer class for whoes to see products and categories and create new Orders
+    """
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
 
+
+class Address(BaseModel):
+    """
+    Adress Class for customer's addresses
+    """
+    province = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    street = models.CharField(max_length=50)
+    alley = models.CharField(max_length=50)
+    number = models.PositiveIntegerField()
+    description = models.TextField(help_text=_("Enter your extra description of your address"))
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
