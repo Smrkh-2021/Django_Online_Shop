93961acb003d8be2a651f83403332cc241dfc2ed
m.kh <khosroshahi89@gmail.com>
2022-02-14 14:41:41 +0330

    add:core app and base model

---
+++ core/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+class BaseManager(models.Manager):
+    """
+    Base Class Manager for Customize the Query Set and Filter by Deleted
+    """
+    def get_queryset(self):
+        return super().get_queryset().filter(is_delete=False)
+
+    def get_all(self):
+        return super().get_queryset()
+
+
+class BaseModel(models.Model):
+    """
+    Basic Class Model for Inheritance All Other Class Model from it
+    """
+    objects = BaseManager()
+    create_datetime = models.DateTimeField(auto_now_add=True, editable=False)
+    modify_datetime = models.DateTimeField(auto_now=True, editable=False)
+    is_active = models.BooleanField(default=True)
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_delete = True
+        self.save()
+
+    def undelete(self):
+        self.is_delete = False
+        self.save()
+
+    def activate(self):
+        self.is_active = True
+        self.save()
+
+    def deactivate(self):
+        self.is_active = False
