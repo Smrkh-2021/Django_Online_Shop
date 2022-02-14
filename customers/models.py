fad0336009574b4de138c9e613259cdf5c701533
m.kh <khosroshahi89@gmail.com>
2022-02-14 15:04:00 +0330

    add: customer model fields

--- customers/models.py
+++ customers/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Customer():
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+
