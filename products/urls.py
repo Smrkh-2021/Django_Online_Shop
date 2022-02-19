20f5695a1fe8daf81861d6bfe654734fb09129f3
m.kh <khosroshahi89@gmail.com>
2022-02-19 04:50:43 +0330

    edit: urls of products

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from .views import ProductListApi, ProductDetailApi, ProductView
 
 
 app_name = 'products'
 
 urlpatterns = [
-
+    path('', ProductView.as_view(), name='product_list_view'),
+    path('product_list_api/', ProductListApi.as_view(), name='product_list_api'),
+    path('product_api/<int:pk>', ProductDetailApi.as_view(), name='product_detail_api'),
 ]
+
