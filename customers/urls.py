6b8ca20ea438838f762f4b109323e2e2d8b8a7f3
m.kh <khosroshahi89@gmail.com>
2022-02-21 12:10:48 +0330

    add: register url

--- customers/urls.py
+++ customers/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import CustomerLoginView
+from .views import CustomerLoginView, CustomerSignupView
 
 # ProductListApi, ProductDetailApi,
 
 app_name = 'customers'
 
 urlpatterns = [
-    path('', CustomerLoginView.as_view(), name='login_view'),
+    path('login/', CustomerLoginView.as_view(), name='login_view'),
+    path('sign up/', CustomerSignupView.as_view(), name='login_view'),
 
 ]
