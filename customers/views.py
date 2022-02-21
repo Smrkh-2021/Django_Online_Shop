8427771cd7f4ca2743b17aa5b44515e9f0dbe2ab
m.kh <khosroshahi89@gmail.com>
2022-02-21 12:11:06 +0330

    add: register view vlass

--- customers/views.py
+++ customers/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import login
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import FormView
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from django.utils.translation import gettext_lazy as _
 from .models import Customer
 # Create your views here.
@@ -24,30 +24,21 @@ class CustomerLoginView(FormView):
         Customer.objects.get_or_create(user=user)
         return super().form_valid(form)
 
-    # def get_context_data(self, **kwargs):
-    #     kwargs['loginform'] = LoginForm()
-    #     kwargs['signupform'] = RegistrationForm()
-    #     return super().get_context_data(**kwargs)
 
 
 #
-# class RegistrationView(FormView):
-#     model_class = RegistrationForm
-#     template_name = 'customers/login.html'
-#     success_url = reverse_lazy('products/home')
-#
-#     def form_valid(self, form):
-#         form.save()
-#         return super().form_valid(form)
-#
-#     def form_invalid(self, form):
-#         return super().form_invalid(form)
+class CustomerSignupView(FormView):
+    form_class = RegistrationForm
+    template_name = 'customers/register.html'
+    success_url = reverse_lazy('products/home')
 
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
-    # def get_context_data(self, **kwargs):
-    #     kwargs['login_form'] = LoginForm()
-    #     kwargs['signup_form'] = RegistrationForm()
-    #     return super().get_context_data(**kwargs)
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
 
 
 
