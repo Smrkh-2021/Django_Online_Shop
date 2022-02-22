ee27b807c5e0f8f61a21d76118e763bfa40ce25c
m.kh <khosroshahi89@gmail.com>
2022-02-22 14:49:39 +0330

    add: ulrs for logout path

--- customers/urls.py
+++ customers/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CustomerLoginView, CustomerSignupView
+from .views import CustomerLoginView, CustomerSignupView, CustomerLogoutView
 
 # ProductListApi, ProductDetailApi,
 
@@ -7,6 +7,7 @@ app_name = 'customers'
 
 urlpatterns = [
     path('login/', CustomerLoginView.as_view(), name='login_view'),
+    path('logout/', CustomerLogoutView.as_view(), name='logout_view'),
     path('register/', CustomerSignupView.as_view(), name='register_view'),
 
 ]
