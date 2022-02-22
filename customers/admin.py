adbd53e54e22ab799b217373077a55ad467b82a8
m.kh <khosroshahi89@gmail.com>
2022-02-22 16:32:09 +0330

    edit: add adress to customer admin panel

--- customers/admin.py
+++ customers/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from.models import *
 # Register your models here.
-admin.site.register(Customer)
+
+class AddressInLine(admin.StackedInline):
+    model = Address
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['fname', 'lname', 'phone', 'email']
+    inlines = [AddressInLine, ]
+
+
+
+admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Address)
