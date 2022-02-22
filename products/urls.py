1543ab47a8be2638ee308fd3338823b1b35f259b
m.kh <khosroshahi89@gmail.com>
2022-02-22 18:06:52 +0330

    add: path for detail product

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import ProductView
+from .views import ProductView, ProductDetailView
 # ProductListApi, ProductDetailApi,
 
 app_name = 'products'
 
 urlpatterns = [
     path('', ProductView.as_view(), name='product_list_view'),
+    path('product/<slug:name>', ProductDetailView.as_view(), name='product_Detail_view'),
     # path('product_list_api/', ProductListApi.as_view(), name='product_list_api'),
     # path('product_api/<int:pk>', ProductDetailApi.as_view(), name='product_detail_api'),
 ]
