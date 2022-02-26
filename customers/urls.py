7f62b4a8257b480fd9711765ddc131c3447d6810
m.kh <khosroshahi89@gmail.com>
2022-02-27 00:24:44 +0330

    add: urls path

--- customers/urls.py
+++ customers/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CustomerLoginView, CustomerSignupView, CustomerLogoutView
+from .views import *
 
 # ProductListApi, ProductDetailApi,
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('login/', CustomerLoginView.as_view(), name='login_view'),
     path('logout/', CustomerLogoutView.as_view(), name='logout_view'),
     path('register/', CustomerSignupView.as_view(), name='register_view'),
-
+    path('user_list/', UserListApi.as_view(), name='user_listview'),
+    path('user_detail/<int:pk>', UserDetailApi.as_view(), name='user_detailview'),
+    path('address_list/', AddressListApi.as_view(), name='address_listview'),
 ]
