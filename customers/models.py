f540c33efa1381b15442baefe262854f680ae210
m.kh <khosroshahi89@gmail.com>
2022-02-27 00:38:20 +0330

    edit: model of customers

--- customers/models.py
+++ customers/models.py
@@ -22,7 +22,7 @@ class Customer(BaseModel):
     phone = models.CharField(max_length=15, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.username}'
+        return f'{self.phone}'
 
 
 class Address(BaseModel):
@@ -33,15 +33,15 @@ class Address(BaseModel):
         verbose_name = _("Address")
         verbose_name_plural = _("Addresses")
 
-    province = models.CharField(max_length=30, verbose_name=_('province'))
-    city = models.CharField(max_length=30, verbose_name=_('city'))
-    street = models.CharField(max_length=50, verbose_name=_('street'))
-    alley = models.CharField(max_length=50, verbose_name=_('alley'))
-    number = models.PositiveIntegerField(verbose_name=_('number'))
+    province = models.CharField(max_length=30, verbose_name=_('province'), null=True, blank=True)
+    city = models.CharField(max_length=30, verbose_name=_('city'), null=True, blank=True)
+    street = models.CharField(max_length=50, verbose_name=_('street'), null=True, blank=True)
+    alley = models.CharField(max_length=50, verbose_name=_('alley'), null=True, blank=True)
+    number = models.PositiveIntegerField(verbose_name=_('number'), null=True, blank=True)
     description = models.TextField(help_text=_("Enter your extra description of your address"), null=True, blank=True, verbose_name=_('description'))
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True, verbose_name=_('customer'))
 
     def __str__(self):
-        return f'Address Customer: {self.customer}'
+        return f'Address Customer: {self.customer} is: {self.description}'
 
 
