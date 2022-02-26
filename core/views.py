74c66e77c13d96118736b186f13fe23331d117ed
m.kh <khosroshahi89@gmail.com>
2022-02-27 00:27:21 +0330

    add: for test logging

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
+import logging
+
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class TestView(View):
+    def get(self):
+        logging.getLogger()
