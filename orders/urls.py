56253b4d199f37da2052eb8fb83d7a9ac568e6aa
m.kh <khosroshahi89@gmail.com>
2022-02-27 00:27:49 +0330

    add: urls to orders path

--- orders/urls.py
+++ orders/urls.py
@@ -1,1 +1,12 @@
+from django.urls import path
+from .views import *
+
+# ProductListApi, ProductDetailApi,
+
+app_name = 'orders'
+
+urlpatterns = [
+    # path('', CartView.as_view(), name='cart_view'),
+
+]
 
