fc7676aa32f543a2733bbc5ee24fdaaeb4cfc610
m.kh <khosroshahi89@gmail.com>
2022-02-27 22:34:12 +0330

    edit: url path for product list category

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductView, ProductDetailView
+from .views import *
 # ProductListApi, ProductDetailApi,
 
 app_name = 'products'
@@ -7,6 +7,7 @@ app_name = 'products'
 urlpatterns = [
     path('', ProductView.as_view(), name='product_list_view'),
     path('product/<int:pk>', ProductDetailView.as_view(), name='product_Detail_view'),
+    path('productlist/', ProductListView.as_view(), name='product_list'),
     # path('product_list_api/', ProductListApi.as_view(), name='product_list_api'),
     # path('product_api/<int:pk>', ProductDetailApi.as_view(), name='product_detail_api'),
 ]
