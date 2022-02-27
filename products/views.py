880f82886336d6d24286ec00ad0014994fbc0cac
m.kh <khosroshahi89@gmail.com>
2022-02-27 22:34:53 +0330

    add: view to show products list category

--- products/views.py
+++ products/views.py
@@ -21,20 +21,48 @@ class ProductView(ListView):
 
 
 class ProductDetailView(DetailView):
+    """
+    class view for display detail of a product
+    """
     model = Product
     template_name = 'products/product_detail.html'
 
 
 
 class ProductListView(ListView):
+    """
+    class view for display products of one category
+    """
     model = Product
     template_name = 'products/product_list.html'
 
+    def get_queryset(self):
+        filters = dict(self.request.GET)
+        try:
+            for key, value in filters.items():
+                filters[key] = ''.join(value)
+            return Product.objects.filter(**filters)
+        except Exception:
+            return super().get_queryset()
+
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     # print('llll',self.request.GET.get(object_list))
+    #     kwargs['category_products'] = Product.objects.filter(category_id=category_id)
+    #     return super().get_context_data(object_list=object_list, **kwargs)
+
+##################################### API View ######################################
 
 
-class ProductListApi(generics.ListCreateAPIView):
+class ProductListApi(generics.ListAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+    def get_queryset(self):
+        cat_id = self.kwargs['pk']
+        return Product.objects.filter(category_id=cat_id)
+
+
+
 
 
 class ProductDetailApi(generics.RetrieveUpdateDestroyAPIView):
