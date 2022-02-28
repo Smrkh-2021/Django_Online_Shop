353396c63459d361d5142f0982bcdf5b8695aa2c
m.kh <khosroshahi89@gmail.com>
2022-02-28 11:59:35 +0330

    add: product admin

--- products/admin.py
+++ products/admin.py
@@ -2,8 +2,22 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'discount', 'count', 'brand', 'category']
+    # list_display_links = ['phone', 'fname']
+    # list_filter = ['phone']
+    # inlines = [AddressInLine, ]
+    # search_fields = ['fname', 'lname']
+
+
+
+
+
+
+
 admin.site.register(Discount)
 admin.site.register(Category)
 admin.site.register(Brand)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Comment)
