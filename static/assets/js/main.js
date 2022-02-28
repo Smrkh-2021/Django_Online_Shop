c7fe24e395f4e4856b6b71a0d098a5932ab90d51
m.kh <khosroshahi89@gmail.com>
2022-02-28 12:02:49 +0330

    convert: Euro sign to dollar sign

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -468,14 +468,14 @@
     $( "#slider-range" ).slider({
         range: true,
         min: 0,
-        max: 500,
-        values: [ 0, 500 ],
+        max: 10000,
+        values: [ 0, 10000 ],
         slide: function( event, ui ) {
-        $( "#amount" ).val( "€" + ui.values[ 0 ] + " - €" + ui.values[ 1 ] );
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
        }
     });
-    $( "#amount" ).val( "€" + $( "#slider-range" ).slider( "values", 0 ) +
-       " - €" + $( "#slider-range" ).slider( "values", 1 ) );
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+       " - $" + $( "#slider-range" ).slider( "values", 1 ) );
     
     /*---niceSelect---*/
      $('.niceselect_option').niceSelect();
