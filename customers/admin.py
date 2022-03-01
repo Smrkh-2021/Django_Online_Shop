8fe42fdfe362944346a243b1dbcc71ac5f2c4d95
m.kh <khosroshahi89@gmail.com>
2022-03-01 17:45:36 +0330

    edit: inheritance of model admins customer

--- customers/admin.py
+++ customers/admin.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
 from.models import *
+from core.admin import LogicalAdminModel
 # Register your models here.
 
 class AddressInLine(admin.StackedInline):
     model = Address
 
 
-class AddressAdmin(admin.ModelAdmin):
+class AddressAdmin(LogicalAdminModel):
     list_display = ['province', 'city', 'street', 'alley', 'number', 'description']
     list_display_links = ['province', 'city', 'description']
     list_filter = ['province', 'city', 'street']
     search_fields = ['province', 'city']
 
 
-class CustomerAdmin(admin.ModelAdmin):
+class CustomerAdmin(LogicalAdminModel):
     list_display = ['fname', 'lname', 'phone', 'email', 'user']
     list_display_links = ['phone', 'fname', 'email']
     list_filter = ['user']
