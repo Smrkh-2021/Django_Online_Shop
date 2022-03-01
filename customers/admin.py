493c883c4f9e50e1961a7d620a16343fac9d1a17
m.kh <khosroshahi89@gmail.com>
2022-03-01 13:35:40 +0330

    edit and add: model admin of address ans customer

--- customers/admin.py
+++ customers/admin.py
@@ -6,14 +6,21 @@ class AddressInLine(admin.StackedInline):
     model = Address
 
 
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['province', 'city', 'street', 'alley', 'number', 'description']
+    list_display_links = ['province', 'city', 'description']
+    list_filter = ['province', 'city', 'street']
+    search_fields = ['province', 'city']
+
+
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['fname', 'lname', 'phone', 'email', 'user']
-    list_display_links = ['phone', 'fname']
-    list_filter = ['phone']
+    list_display_links = ['phone', 'fname', 'email']
+    list_filter = ['user']
     inlines = [AddressInLine, ]
     search_fields = ['fname', 'lname']
 
 
 
 admin.site.register(Customer, CustomerAdmin)
-admin.site.register(Address)
+admin.site.register(Address, AddressAdmin)
