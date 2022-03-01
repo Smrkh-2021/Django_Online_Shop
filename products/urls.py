5df2ebc4c9ced7d702d2d10bf21fe18b93189272
m.kh <khosroshahi89@gmail.com>
2022-03-01 13:47:45 +0330

    remove: extra comment

--- products/urls.py
+++ products/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('', ProductView.as_view(), name='product_list_view'),
     path('product/<int:pk>', ProductDetailView.as_view(), name='product_Detail_view'),
     path('productlist/', ProductListView.as_view(), name='product_list'),
-    # path('product_list_api/', ProductListApi.as_view(), name='product_list_api'),
-    # path('product_api/<int:pk>', ProductDetailApi.as_view(), name='product_detail_api'),
 ]
 
