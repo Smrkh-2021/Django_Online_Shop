6367e02798f00c369c05d24bd80910a1605161b8
m.kh <khosroshahi89@gmail.com>
2022-03-01 17:44:57 +0330

    create: class for logicaladminmodel for all admin models

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,14 @@ from django.contrib.auth.models import Group
 from .models import User
 # Register your models here.
 
+class LogicalAdminModel(admin.ModelAdmin):
+    """
+    custom admin model for logical options
+    """
+    def logical_delete(self, request, queryset):
+        queryset.update(is_delete=True)
 
+    actions = ['logical_delete']
 
 
 # admin.site.register(Group, GroupAdmin)
