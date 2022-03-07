78935f289c968ce8f76af8524ae3f410df3e963c
m.kh <khosroshahi89@gmail.com>
2022-03-08 02:17:15 +0330

    create: api_urls for orders app

--- orders/api_urls.py
+++ orders/api_urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .api_views import OrderItemViewSet
 from .views import *
 
 
+router = DefaultRouter()
+router.register('order_item', OrderItemViewSet)
 
 
 urlpatterns = [
+    path("", include(router.urls))
     # path('product_list_api/', ProductListCreateApi.as_view(), name='product_list_api'),
-    path('order_api/<int:pk>', OrderDetailApi.as_view(), name='order_detail_api'),
+    # path('order_api/<int:pk>', OrderDetailApi.as_view(), name='order_detail_api'),
 ]
