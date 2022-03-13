388d1e43fb16f8b01cb7df831badee62070924ad
m.kh <khosroshahi89@gmail.com>
2022-03-13 18:22:28 +0330

    comment: extra path urls

--- orders/api_urls.py
+++ orders/api_urls.py
@@ -1,16 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .api_views import OrderItemViewSet
+from .api_views import OrderItemViewSet, OrderViewSet
 from .views import *
 
 
 router = DefaultRouter()
 router.register('orderitem', OrderItemViewSet)
+router.register('order', OrderViewSet)
+
+urlpatterns = router.urls
 
 
-urlpatterns = [
-    path("", include(router.urls)),
-    # path('product_list_api/', ProductListCreateApi.as_view(), name='product_list_api'),
-    # path('order_api/<int:pk>', OrderDetailApi.as_view(), name='order_detail_api'),
-]
+# urlpatterns = [
+#     path("", include(router.urls)),
+#     path("", include(router.urls)),
+#     # path('product_list_api/', ProductListCreateApi.as_view(), name='product_list_api'),
+#     # path('order_api/<int:pk>', OrderDetailApi.as_view(), name='order_detail_api'),
+# ]
