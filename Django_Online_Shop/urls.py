0998e9a2a6f9603c7142fb4f0604d9083895f7ca
m.kh <khosroshahi89@gmail.com>
2022-04-03 02:29:14 +0430

    add: i18n and paths

--- Django_Online_Shop/urls.py
+++ Django_Online_Shop/urls.py
@@ -17,10 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 
-
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('api/products', include('products.api_urls')),
@@ -29,5 +29,10 @@ urlpatterns = [
     path('rosetta/', include('rosetta.urls')),
     # path('customers/', include('customers.urls')),
     path('orders_api/', include('orders.api_urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('customers_api/', include('customers.api_urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+handler404 = 'core.views.handler_404'
 
