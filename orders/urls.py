8430cb4f5b354e8a7169d25dbea42a566f281963
m.kh <khosroshahi89@gmail.com>
2022-04-03 02:28:10 +0430

    comment: extra urls

--- orders/urls.py
+++ orders/urls.py
@@ -7,7 +7,7 @@ app_name = 'orders'
 
 urlpatterns = [
     path('', OrderItemListView.as_view(), name='orderitem_list'),
-    path('delitem/<int:pk>', OrderItemDeleteView.as_view(), name='orderitem_delete'),
+    # path('delitem/<int:pk>', OrderItemDeleteView.as_view(), name='orderitem_delete'),
     path('panelorders/', OrderListView.as_view(), name='panel_orders_list')
 ]
 
