99753dc917b9429b4d436053aa6b3d5543c1b512
m.kh <khosroshahi89@gmail.com>
2022-04-03 01:54:44 +0430

    add: orderitem_num function for number of order_items

--- orders/context_processors.py
+++ orders/context_processors.py
@@ -4,7 +4,30 @@ from customers.models import Customer
 from orders.models import Order
 
 
-# def cart_item(request):
-#     user = request.User
-#     if user.is_authenticated:
-#         ...
+def orderitem_num(request):
+    if request.user.is_anonymous:
+        cookie_str = request.COOKIES.get('cookie_product')
+        if cookie_str:
+            cookie_dict = json.loads(cookie_str)
+            orderitem_num = len(cookie_dict)
+            return {
+                    'orderitem_num': orderitem_num
+            }
+        else:
+            return {
+                    'orderitem_num': 0
+            }
+
+    else:
+        user = request.user
+        customer = Customer.objects.get(user=user)
+        try:
+            order = Order.objects.get(customer=customer, status_id=3)
+        except:
+            return {
+                'orderitem_num': 0
+            }
+        orderitem_num = len(order.orderitem_set.all())
+        return {
+            'orderitem_num': orderitem_num
+        }
