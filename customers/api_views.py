ab9c14a66249239a31852893654d7e6b38f0b1b3
m.kh <khosroshahi89@gmail.com>
2022-04-04 19:19:22 +0430

    override: update method for password and ...

--- customers/api_views.py
+++ customers/api_views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import PasswordChangeForm
 from django.http import JsonResponse
 from django.template.loader import render_to_string
 from rest_framework import viewsets
+from rest_framework.response import Response
+
 from .models import Address, Customer
 from customers.serializers import *
 
@@ -35,6 +38,7 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    print('areeeeeee')
 
     def list(self, request, *args, **kwargs):
         user = request.user
@@ -43,6 +47,24 @@ class UserViewSet(viewsets.ModelViewSet):
         }
         template_string = render_to_string(template_name='customers/panel_profile.html', context=context)
         return JsonResponse({'user':template_string})
+
+    def update(self, request, *args, **kwargs):
+        print(self.request.data)
+        if self.request.data.get('old_password'):
+            old_password = self.request.data['old_password']
+            print('old_password:', old_password)
+            new_password = self.request.data['new_password1']
+            confirm_password = self.request.data['new_password2']
+            form = PasswordChangeForm(self, {'old_password': old_password, 'new_password1': new_password,
+                                             'new_password2': confirm_password})
+            if form.is_valid():
+                form.save()
+                return Response(status=200)
+            else:
+                return Response({'errors': form.errors.values()}, status=400)
+        return super().update(request, *args, **kwargs)
+
+
 
 
 
