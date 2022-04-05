9628abb4ca9ea35f29a541be8a2a2cbcb36e14e1
m.kh <khosroshahi89@gmail.com>
2022-04-05 13:11:25 +0430

    remove: extra field from customer model

--- customers/models.py
+++ customers/models.py
@@ -15,11 +15,8 @@ class Customer(BaseModel):
         verbose_name_plural = _("Customers")
 
     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
-    fname = models.CharField(max_length=50, null=True, blank=True)
-    lname = models.CharField(max_length=50, null=True, blank=True)
-    username = models.CharField(max_length=50, null=True, blank=True)
-    email = models.EmailField(null=True, blank=True)
-    phone = models.CharField(max_length=15, null=True, blank=True)
+
+
 
     def __str__(self):
         return f'{self.phone}'
