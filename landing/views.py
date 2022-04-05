b8dba2fc45f40ca17fbc1db8eb15b50a5ae8ad22
m.kh <khosroshahi89@gmail.com>
2022-04-05 13:05:37 +0430

    add: view for show contact us page

--- landing/views.py
+++ landing/views.py
@@ -4,4 +4,34 @@ from django.views.generic import TemplateView
 from landing.forms import ContactUsForm
 from django.shortcuts import render
 
+
+
+
+
 # Create your views here.
+
+
+
+class ContactUsView(FormView):
+    form_class = ContactUsForm
+    success_url = 'contactus'
+    template_name = 'landing/contact.html'
+
+    def form_valid(self, form):
+        print('ooomaaaad')
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        subject = form.cleaned_data['subject']
+        message = form.cleaned_data['message']
+        send_mail(subject, message, email, ['m64.django@gmail.com', email])
+        print('done')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        print('form invalid')
+        print(form.errors)
+        return super().form_invalid(form)
+
+
+class AboutView(TemplateView):
+    template_name = "landing/about.html"
