f21739fa6ca31236e6a01261fd8448d4fa098c96
m.kh <khosroshahi89@gmail.com>
2022-04-05 13:04:54 +0430

    add: imported libraries

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,7 @@
+from django.core.mail import send_mail
+from django.views.generic import FormView
+from django.views.generic import TemplateView
+from landing.forms import ContactUsForm
 from django.shortcuts import render
 
 # Create your views here.
