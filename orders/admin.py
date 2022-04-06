b428f745e6929c871025fc1df9857e64c450aa27
m.kh <khosroshahi89@gmail.com>
2022-04-06 22:03:45 +0430

    add: status link in order admin

--- orders/admin.py
+++ orders/admin.py
@@ -19,9 +19,9 @@ class OffCodeAdmin(LogicalAdminModel):
 
 class OrderAdmin(LogicalAdminModel):
     list_display = ['address', 'offcode', 'status', 'customer', 'final_price', 'total_price']
-    list_display_links = ['address', 'offcode', 'customer']
-    list_filter = ['customer', 'address']
-    search_fields = ['address', 'customer']
+    list_display_links = ['address', 'offcode', 'customer', 'status']
+    list_filter = ['customer', 'address', 'status']
+    search_fields = ['address', 'customer', 'status']
 
 
 class OrderItemAdmin(LogicalAdminModel):
