780ad16baa40bcfcc33a99ddf246dc0584d6ac78
m.kh <khosroshahi89@gmail.com>
2022-04-06 22:04:46 +0430

    add: last new product for show in home page

--- products/views.py
+++ products/views.py
@@ -19,6 +19,7 @@ class ProductView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         kwargs['root_categories'] = Category.objects.filter(parent=None)
+        kwargs['last_products'] = Product.objects.order_by('-id')[:7]
         return super().get_context_data(object_list=object_list, **kwargs)
 
 
