4e7d20de9ce3e60e41bec15baa0e5645b51eea66
m.kh <khosroshahi89@gmail.com>
2022-04-07 00:04:34 +0430

    modify: def orderitem_num function

--- orders/context_processors.py
+++ orders/context_processors.py
@@ -5,31 +5,24 @@ from orders.models import Order
 
 
 def orderitem_num(request):
-    if request.user.is_anonymous:
-        cookie_str = request.COOKIES.get('cookie_product')
-        if cookie_str:
+    try:
+        if request.user.is_anonymous:
+            cookie_str = request.COOKIES.get('cookie_product')
             cookie_dict = json.loads(cookie_str)
             orderitem_num = len(cookie_dict)
             return {
                     'orderitem_num': orderitem_num
             }
-        else:
-            return {
-                    'orderitem_num': 0
-            }
-
-    else:
         user = request.user
         customer = Customer.objects.get(user=user)
-        try:
-            order = Order.objects.get(customer=customer, status_id=3)
-        except:
-            return {
-                'orderitem_num': 0
-            }
+        order = Order.objects.get(customer=customer, status_id=3)
         orderitem_num = len(order.orderitem_set.all())
         return {
             'orderitem_num': orderitem_num
+        }
+    except:
+        return {
+            'orderitem_num': 0
         }
 
 
